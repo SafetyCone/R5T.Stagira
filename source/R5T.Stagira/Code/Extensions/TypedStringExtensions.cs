3d969decb3bfe533894ad1c2b3d9e4a9052cc78e
MinexAutomation <minex.automation@gmail.com>
2020-05-06 23:16:19 -0700

    Added ease-of-use extension method ValueEquals() to test if the values of two different TypedStrings are equal (for comparing the values of two different derived TypedStrings).

---
+++ source/R5T.Stagira/Code/Extensions/TypedStringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Stagira
+{
+    public static class TypedStringExtensions
+    {
+        public static bool ValueEquals(TypedString a, TypedString b)
+        {
+            var valueEquals = a.Value.Equals(b.Value);
+            return valueEquals;
+        }
+    }
+}
