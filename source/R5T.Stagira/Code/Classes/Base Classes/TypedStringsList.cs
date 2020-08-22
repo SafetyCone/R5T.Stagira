87835d5c132005206ed2cbf4d5ffd1553826d181
David Coats <david@getsafetycone.com>
2020-08-22 10:13:13 -0700

    Add TypedStringsList type

---
+++ source/R5T.Stagira/Code/Classes/Base Classes/TypedStringsList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Stagira
+{
+    public abstract class TypedStringsList<T>
+        where T : TypedString
+    {
+        public List<T> Values { get; } = new List<T>();
+
+
+        public TypedStringsList()
+        {
+        }
+
+        public TypedStringsList(IEnumerable<T> values)
+        {
+            this.Values.AddRange(values);
+        }
+    }
+}
